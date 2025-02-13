4ded48fcfbd4902d5a68898d93c7549065a472ba
bion-slmn <bionsol25@gmail.com>
2025-02-13 21:25:20 +0300

    updated the seed database to have all books available

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,9 @@ Book.destroy_all
 # Seed the database with sample books
 books = [
   { title: "The Great Gatsby", author: "F. Scott Fitzgerald", isbn: "9780743273565", status: 'available' },
-  { title: "1984", author: "George Orwell", isbn: "9780451524935", status: 'borrowed' },
+  { title: "1984", author: "George Orwell", isbn: "9780451524935", status: 'available' },
   { title: "To Kill a Mockingbird", author: "Harper Lee", isbn: "9780061120084", status: 'available' },
-  { title: "Pride and Prejudice", author: "Jane Austen", isbn: "9781503290563", status: 'borrowed '},
+  { title: "Pride and Prejudice", author: "Jane Austen", isbn: "9781503290563", status: 'available'},
   { title: "Moby-Dick", author: "Herman Melville", isbn: "9781503280786", status: 'available' }
 ]
 
